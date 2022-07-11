d44d2a55c5cb31a9ae7fe28139cea6d3eed9b4e6
Ethan Standel <ethan@standel.io>
2022-07-11 02:28:56 -0400

    remove top-bar from scroll-space

--- app/src/main/java/io/standel/jetpackernews/components/Layout.kt
+++ app/src/main/java/io/standel/jetpackernews/components/Layout.kt
@@ -4,34 +4,44 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Layout() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        "JetpackerNews",
-                        color = MaterialTheme.colorScheme.onPrimary
-                    )
-                },
-                colors = TopAppBarDefaults.smallTopAppBarColors(
-                    MaterialTheme.colorScheme.primary
-                ),
-            )
-            Column(modifier = Modifier.weight(1F)) {
-                Stories()
-            }
-            BottomAppBar {
+    val topBarScrollBehavior = remember { TopAppBarDefaults.enterAlwaysScrollBehavior() }
 
+    Scaffold(
+        modifier = Modifier.nestedScroll(topBarScrollBehavior.nestedScrollConnection),
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(
+                            "JetpackerNews",
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                    },
+                    colors = TopAppBarDefaults.smallTopAppBarColors(
+                        MaterialTheme.colorScheme.primary
+                    ),
+                    scrollBehavior = topBarScrollBehavior
+                )
+                Column(modifier = Modifier.weight(1F)) {
+                    Stories()
+                }
+                BottomAppBar {
+
+                }
             }
         }
     }
