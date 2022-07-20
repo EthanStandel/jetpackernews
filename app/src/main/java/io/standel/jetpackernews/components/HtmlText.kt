16dc70dd9cafc3d6b81da7b63b3f82600298dc53
Ethan Standel <ethan@standel.io>
2022-07-20 09:35:14 -0400

    removes the stupid fruits of my stupid labor

--- app/src/main/java/io/standel/jetpackernews/components/HtmlText.kt
+++ app/src/main/java/io/standel/jetpackernews/components/HtmlText.kt
@@ -3,9 +3,9 @@ package io.standel.jetpackernews.components
 import android.widget.TextView
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
-import io.standel.jetpackernews.utils.toLegacyColorInt
 
 @Composable
 fun HtmlText(html: String) {
@@ -14,7 +14,7 @@ fun HtmlText(html: String) {
     AndroidView(
         factory = { context ->
             TextView(context).apply {
-                setTextColor(textColor.toLegacyColorInt())
+                setTextColor(textColor.toArgb())
                 text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_LEGACY)
             }
         },
